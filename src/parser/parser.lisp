b2f85a75aa407a7061636c8b53cb0cc40838f7e3
Job <lambdainsphere@Jobs-MacBook-Air.local>
2022-04-27 18:13:58 -0700

    add documentation.

--- src/parser/parser.lisp
+++ src/parser/parser.lisp
@@ -1,6 +1,7 @@
 (in-package #:manifold-scheme)
 
 (defun parse-exp (exp)
+  "Parse given EXP."
   (match exp
 	 ((guard x (and (not (scm-boolp x))
 			(symbolp x)))
@@ -36,81 +37,176 @@
 	 
          (t error "Not valid expression")))
 
-(defstruct var v)
+(defstruct var
+  "A Variable Node."
+  v)
 
-(defstruct int i)
+(defstruct int
+  "An Int Node."
+  i)
 
-(defstruct bool value)
+(defstruct bool
+  "A Boolean Node."
+  value)
 
-(defstruct letscm var expr body)
+(defstruct letscm
+  "A Let Node."
+  var expr body)
 
-(defstruct ifscm cond then else)
+(defstruct ifscm
+  "An If Node."
+  cond then else)
 
-(defstruct lambdascm var body)
+(defstruct lambdascm
+  "A Lambda Node."
+  var body)
 
-(defstruct setscm var rhs)
+(defstruct setscm
+  "A Set! Node."
+  var rhs)
 
 (defstruct primitive
+  "A Primitive Node."
   op
   operands)
 
+;;; An Exp is one of:
+;;;     - bool
+;;;     - let
+;;;     - set!
+;;;     - if
+;;;     - lambda
+;;;     - primitive
+
 (defun scm-boolp (exp)
+  "Check if Exp is a boolean."
+  ;; Exp -> bool
+  ;; given: (scm-boolp 'true)
+  ;; expect: T
   (and (not (listp exp))
        (or (equalp exp 'true)
 	   (equalp exp 'false))))
 
 (defun scm-letp (exp)
+  "Check if Exp is a Let expression."
+  ;; Exp -> bool
+  ;; given: (scm-letp '(let ((n 2)) (* n n)))
+  ;; expect: T
   (and (listp exp)
        (equalp (car exp) 'let)))
 
 (defun let-var (exp)
+  "Get the variable of the Let Exp."
+  ;; Exp -> Exp
+  ;; given: (let-var '(let ((n 2)) (* n n)))
+  ;; expect: n
   (car (car (car (cdr exp)))))
 
 (defun let-exp (exp)
+  "Get the exp of the Let Exp."
+  ;; Exp -> Exp
+  ;; given: (let-exp '(let ((n 2)) (* n n)))
+  ;; expect: 2
   (car (cdr (car (car (cdr exp))))))
 
 (defun let-body (exp)
+  "Get the body of the Let Exp."
+  ;; Exp -> Exp
+  ;; given: (let-body '(let ((n 2)) (* n n)))
+  ;; expect: (* n n)
   (car (cdr (cdr exp))))
 
 (defun scm-setp (exp)
+  "Check if Exp is a Set! expression."
+  ;; Exp -> bool
+  ;; given: (scm-setp '(set! d 2))
+  ;; expect: T
   (and (listp exp)
        (equalp (car exp) 'set!)))
 
 (defun set-var (exp)
+  "Get the variable of the Set! expression."
+  ;; Exp -> Exp
+  ;; given: (set-var '(set! d 2))
+  ;; expect: d
   (car (cdr exp)))
 
 (defun set-rhs (exp)
+  "Get the right hand side of the Set! expression."
+  ;; Exp -> Exp
+  ;; given: (set-rhs '(set! d 2))
+  ;; expect: 2
   (car (cdr (cdr exp))))
 
 (defun scm-ifp (exp)
+  "Check if the Exp is an If expression."
+  ;; Exp -> bool
+  ;; given: (scm-ifp '(if (= 2 2) 1 2))
+  ;; expect: T
   (and (listp exp)
        (equalp (car exp) 'if)))
 
 (defun if-cond (exp)
+  "Get the cond of the If expression."
+  ;; Exp -> Exp
+  ;; given: (if-cond '(if (= 2 2) 1 2))
+  ;; expect: (= 2 2)
   (car (cdr exp)))
 
 (defun if-then (exp)
+  "get the consequent of the If expression."
+  ;; Exp -> Exp
+  ;; given: '(if-then '(if (= 2 2) 1 2))
+  ;; expect: 1
   (car (cdr (cdr exp))))
 
 (defun if-else (exp)
+  "get the else of the If expression."
+  ;; Exp -> Exp
+  ;; given: '(if-then '(if (= 2 2) 1 2))
+  ;; expect: 2
   (car (cdr (cdr (cdr exp)))))
 
 (defun scm-lambdap (exp)
+  "Checks if the Exp is a Lambda expression."
+  ;; Exp -> bool
+  ;; given: (scm-lambdap '(lambda (n) (* n n)))
+  ;; T
   (and (listp exp)
        (equalp (car exp) 'lambda)))
 
 (defun lambda-var (exp)
+  "Checks if the Exp is a Lambda expression."
+  ;; Exp -> Exp 
+  ;; given: (lambda-var '(lambda (n) (* n n)))
+  ;; (n)
   (car (cdr exp)))
 
 (defun lambda-body (exp)
+  "Checks if the Exp is a Lambda expression."
+  ;; Exp -> Exp 
+  ;; given: (lambda-body '(lambda (n) (* n n)))
+  ;; (* n n)
   (car (cdr (cdr exp))))
 
 (defun scm-primitive-p (exp)
+  "Checks if the Exp is a primitive."
+  ;; Exp -> bool
+  ;; given: (scm-primitive-p '(* 2 2))
+  ;; T
   (listp exp))
   
 
 (defun prim-op (exp)
+  "Get the operator of the Primitive expression."
+  ;; Exp -> Exp
+  ;; given: (prim-op '(* 2 2))
+  ;; *
   (car exp))
 
 (defun prim-operands (exp)
+  "Get the operator of the Primitive expression."
+  ;; Exp -> Exp
+  ;; given: (prim-operands '(* 2 2))
+  ;; (2 2)
   (cdr exp))
