c952188870cc639ce421e52109bd1ebe56820b78
Job <lambdainsphere@Jobs-MacBook-Air.local>
2022-05-04 17:08:00 -0700

    add desugaring for letrec and begin.

--- src/desugar.lisp
+++ src/desugar.lisp
@@ -24,11 +24,11 @@
 	 ((guard x (letscm-p x))
 	  (desugar (desugar-letscm x)))
 
-	 ;;((guard x (letrecscm-p x))
-	 ;; (desugar (desugar-letrecscm x)))
+	 ((guard x (letrecscm-p x))
+	  (desugar (desugar-letrecscm x)))
 
-	 ;;((guard x (beginscm-p x))
-	  ;;(desugar (desugar-beginscm x)))
+	 ((guard x (beginscm-p x))
+	  (desugar (desugar-beginscm x)))
 
 	 ((guard x (primitive-p x))
 	  (make-primitive :op (primitive-op x)
@@ -38,7 +38,9 @@
 	 ((guard x (application-p x))
 	  (make-application :operator (desugar (application-operator x))
 			    :operands (mapcar #'(lambda (e) (desugar e))
-					      (application-operands x))))
+					      (if (listp (application-operands x))
+						  (application-operands x)
+						(list (application-operands x))))))
 
 	 (_ (error "Invalid Expression"))))
 
@@ -51,5 +53,43 @@
 	   :operands b))
 	 (_ (error "Not a Let expression"))))
 
+(defun desugar-letrecscm (ast)
+  (match ast
+	 ((letrecscm :bindings a :expression b)
+	  (let* ((namings (mapcar (lambda (b) (list (car b) (parse-exp 'false)))
+				  a))
+		 (sets (mapcar (lambda (binding)
+				 (make-setscm :var (car binding)
+					      :rhs (car (cdr binding))))
+			       a))
+		 (vars (mapcar (lambda (binding) (car binding))
+			       namings))
+		 (arguments (mapcar (lambda (binding) (car (cdr binding)))
+			       namings)))
+	    (make-letscm :var vars
+			 :expr arguments
+			 :body (make-beginscm :body sets
+					      :expression b))))
+
+	 (_ (error "Not a Letrec expression."))))
+
 
 	 
+(defun desugar-beginscm (ast)
+  (match ast
+	 ((beginscm :body a :expression b)
+	  (let ((exps (append a (list b))))
+	    (make-letbind exps)))
+
+	 (_ (error "Not a Begin expression."))))
+  
+
+(defun make-letbind (exps)
+  (if (and (listp exps)
+	   (equalp (length exps) 1))
+      (car exps)
+    (make-letscm :var (gensym "$K")
+		 :expr (car exps)
+		 :body (make-letbind (cdr exps)))))
+    
+  
